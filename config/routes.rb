6092269476f90f64259430cd92b61c4e0196f086
Gabriel Chazanov <gabe.chaz@gmail.com>
2020-12-14 14:42:40 -0500

    This is a test commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,7 @@ Rails.application.routes.draw do
   resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+def say hello
+  puts "say hello!"
+end
