95c47a17f324be96bd5eac943c0b265a05071326
Conor Murnane <conormurnane11@gmail.com>
2020-12-14 16:23:21 -0500

    methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   # resources :dogs
   get '/dogs', to: 'dogs#index', as: 'dogs'
   get '/dog/:id', to: 'dogs#show', as: 'dog'
-
-  resources :employees
+  get '/employees', to: 'employees#index', as: 'employees'
+  get '/employees/:id', to: 'employees#show', as: 'employee'
+  get '/employees/new', to: 'employees#new', as: 'new_employee'
+  post '/employees', to: 'employees#create'
+  get '/employees/:id/edit', to: 'employees#edit', as: 'edit_employee'
+  patch '/employees/:id/update', to: 'employees#update'
+  
+  #resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
